2d748bb98165c577c5888282f9332a4bbd72c9b9
skyfeeder <skylifealex@gmail.com>
2023-05-13 10:09:04 +0600

    refactored to tests directory

--- src/test/java/TrianglePuzzleTest.java
+++ src/test/java/TrianglePuzzleTest.java
@@ -1,9 +1,19 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TrianglePuzzleTest {
-    public static void main(String[] args) {
+
+    @Test
+    public void Test() {
+
+        boolean isDisplayedAnswersLink = false;
+        boolean isDisplayedHideAnswersButton = false;
+
         WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver();
@@ -11,29 +21,26 @@ public class TrianglePuzzleTest {
         driver.get("https://playground.learnqa.ru/puzzle/triangle");
 
         WebElement giveUpButton = driver.findElement(By.xpath("//button[contains(text(),'Я сдаюсь')]"));
-
-        if (giveUpButton.isDisplayed()) {
+        boolean isDisplayedGiveUpButton = giveUpButton.isDisplayed();
+        if (isDisplayedGiveUpButton) {
             giveUpButton.click();
 
             WebElement answersLink = driver.findElement(By.linkText("Ссылка на ответы"));
 
-            if (answersLink.isDisplayed()) {
+            isDisplayedAnswersLink = answersLink.isDisplayed();
+            if (isDisplayedAnswersLink) {
+
                 System.out.println("Ссылка на ответы найдена");
 
                 WebElement hideAnswersButton = driver.findElement(By.xpath("//button[contains(text(),'Спрятать ответы')]"));
 
-                if (hideAnswersButton.isDisplayed()) {
-                    System.out.println("Кнопка 'Спрятать ответы' найдена");
-                } else {
-                    System.out.println("Кнопка 'Спрятать ответы' не найдена");
-                }
-            } else {
-                System.out.println("Ссылка на ответы не найдена");
+                isDisplayedHideAnswersButton = hideAnswersButton.isDisplayed();
+
             }
-        } else {
-            System.out.println("Кнопка 'Я сдаюсь' не найдена");
         }
 
+        assertTrue(isDisplayedGiveUpButton);
+        assertTrue(isDisplayedHideAnswersButton);
         driver.quit();
     }
 }
